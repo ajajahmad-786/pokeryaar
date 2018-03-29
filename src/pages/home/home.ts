35369ae7c42c963faa494363fd306fb0d7ed56d1
ajajahmad-786 <37896491+ajajahmad-786@users.noreply.github.com>
2018-03-29 16:34:43 +0530

    dasd
    asds

---
+++ src/pages/home/home.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'index.html'
+})
+export class HomePage implements OnInit{ constructor(public navCtrl: NavController,private iab: InAppBrowser) { } 
+ngOnInit()
+{ 
+const browser = this.iab.create('https://pokeryaar-app-betaslot.azurewebsites.net/applogin/','_self',{location:'no'}); 
+} 
+} 
+
